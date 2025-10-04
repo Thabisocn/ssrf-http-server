2570accbb5395dddf23ff24cf9e32f6680e54cc0
xxiii <thabisoc123@gmail.com>
2025-10-05 01:06:08 +0200

    25th commit

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,24 @@
 import type { Server } from "bun"
+import * as fs from 'fs';
+
 
 export default {
 async fetch(request: Request, server: Server) {
     let text = "SSRF test!\n"
-    const requestHeaders = new Headers(request.headers);
-
     text += `\nurl: ${request.url}\n`
+    // relative path
+    const filePath: string = 'test.txt';
+
+    const content: string = fs.readFileSync(filePath, 'utf-8');
+    console.log("CTF:");
+    console.log(content);
 
     for (const [key, value] of request.headers.entries()) {
       if (!key.startsWith("x-vercel-i")) continue
       text += `\n${key}: ${value}`
     }
 
+    //DNS QUERIES
 
     return new Response(text, {
       status: 200,
